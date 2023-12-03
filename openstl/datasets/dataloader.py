8051c4af174ffb282dff533c425fa791c489dd14
rss9311 <115175278+rss9311@users.noreply.github.com>
2023-12-03 11:39:14 -0500

    Update dataloader.py
    adding moving physics

--- openstl/datasets/dataloader.py
+++ openstl/datasets/dataloader.py
@@ -38,6 +38,9 @@ def load_data(dataname, batch_size, val_batch_size, num_workers, data_root, dist
     elif dataname == 'taxibj':
         from .dataloader_taxibj import load_data
         return load_data(batch_size, val_batch_size, data_root, num_workers, **cfg_dataloader)
+    elif dataname == 'movingphysics':
+        from .dataloader_movingphy import load_data
+        return load_data(batch_size, val_batch_size, data_root, num_workers, **cfg_dataloader)
     elif 'weather' in dataname:  # 'weather', 'weather_t2m', etc.
         from .dataloader_weather import load_data
         data_split_pool = ['5_625', '2_8125', '1_40625']
